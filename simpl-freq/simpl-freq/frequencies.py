6dd5b17d919d5ece1a2a325aa21d4c36cc2dbd30
sayon <igorjirkov@gmail.com>
2014-11-05 15:43:18 +0400

    now calculates words frequencies

--- simpl-freq/simpl-freq/frequencies.py
+++ simpl-freq/simpl-freq/frequencies.py
@@ -1,21 +1,56 @@
 import sys
 
+
+from itertools import chain
+
+def getOrDef(dic, key):
+    if not key in dic.keys():
+        return 0
+    return dic[key]
+
+
 if len(sys.argv) ==0:
     print "Usage: python frequencies file1 file2 ...\n The result is stored in out.csv"
 else:
     out = open("out.csv", "w")
-    out.write(','.join(map(str, range(0, 255))) + "\n")
+    keys = []
+
+    encountered = set()
+    freqs = []
     for arg in sys.argv[1:]:
+        print("doing work on " + arg + "\n")
         try:
             f = open(arg, 'r')
-            freq = [0] * 256
-            for c in f.read():
-                if c != ' ' and c != '\t' and c != '\n':
-                    freq[ord(c)] = freq[ord(c)] + 1
+            freq = dict()
+
+            conts =  f.read()
+            print("Contents:" + conts + "\n")
             f.close()
-            out.write(','.join(map(str, freq))  + "\n")
+
+            #character frequencies
+            for c in conts:
+                if c != ' ' and c != '\t' and c != '\n':
+                    encountered.add(c)
+                    freq[c] = getOrDef(freq,c) + 1
+
+            for w in filter(None, ''.join(conts).split(' ')):
+                encountered.add(w)
+                freq[w] = getOrDef(freq, w) + 1
+
+            freqs.append(freq)
+
         except IOError as e:
             print(e)
             print("Can't open file " + arg)
+
+
+    out.write(','.join(encountered) + "\n")
+    for i in xrange(0, len(sys.argv)-1):
+        s = ""
+        for w in encountered:
+            s = s + str(getOrDef(freqs[i], w)) + ","
+        out.write(s[0:-1] + "\n")
+
+    #     out.write(','.join(map(lambda w : getOrDef(freqs[i], w),encountered)) + '\n')
 
     out.close()
