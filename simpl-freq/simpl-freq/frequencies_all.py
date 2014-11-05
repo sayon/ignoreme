10b4f5db158a5fa97e2580e1c93f2293764b1fb8
sayon <igorjirkov@gmail.com>
2014-11-05 19:48:28 +0400

    Correct final script file

---
+++ simpl-freq/simpl-freq/frequencies_all.py
@@ -0,0 +1,80 @@
+import os
+from os.path import join
+import re
+import mmap
+
+def quote(s):
+    s = str(s)
+    if ("\"" in s): s = s.replace("\"", "\"\"")
+    if ( "," in s or ";" in s):
+        return "\"" + s + "\""
+    else:
+        return s
+
+
+def getOrZero(dic, key):
+    if not key in dic.keys():
+        return 0
+    return dic[key]
+
+
+
+def countlines(f):
+    buf = mmap.mmap(f.fileno(), 0)
+    lines = 0
+    readline = buf.readline
+    while readline():
+        lines += 1
+    return lines
+
+def readList(filename):
+    f = open(filename)
+    res = list(map(lambda w: w.replace("\n", ""), f.readlines()))
+    f.close()
+    return res
+
+
+languages = readList("langs.txt")
+results = dict()
+
+for language in languages:
+    print "Language: " , language
+    files = [ f for f in os.listdir(language) if os.path.isfile(join(language,f)) ]
+    print "Files: ", files
+
+    kws = dict()
+    for kw in readList(language+"_keywords.txt"):
+        kws[kw] = 0
+
+
+    for filename in files:
+        features = dict()
+        file = open(language + "/" + filename, 'r')
+        contents = file.read()
+        length_total = 0
+        for kw in kws.keys():
+            features[kw] = len( re.findall(kw, contents ))
+        for sym in contents:
+            if sym != ' ' and sym != '\t' and sym != '\n':
+                features[sym] = getOrZero(features, sym ) + 1
+                length_total += 1
+
+        file.close()
+        results[filename] = (features, language)
+
+
+allfeatures = reduce(lambda x, y: x | set(y[0].keys()) , results.values(), set())
+
+out = open("out.csv", "w")
+
+out.write("name," + ",".join(map(quote, allfeatures)) + "\n")
+
+for entry in results.keys():
+    out.write(results[entry][1])
+    for feature in allfeatures:
+        out.write("," + str(getOrZero(results[entry][0], feature)))
+    out.write("\n")
+
+
+out.close()
+
