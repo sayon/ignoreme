4359aed09d17173408a5f89dcbddba8ca63ea54c
sayon <igorjirkov@gmail.com>
2014-11-06 18:18:19 +0400

    counting keywords and symbols

--- lexers/utils/frequencies_kw_input.py
+++ lexers/utils/frequencies_kw_input.py
@@ -7,7 +7,6 @@ def readList(filename):
     f.close()
     return res
 
-
 def countKeywords(kws, filename):
     count = dict()
     for kw in kws:
@@ -23,8 +22,35 @@ def countKeywords(kws, filename):
     return count
 
 
+
+def countKeywordsAndSymbols(kws, filename):
+    count = dict()
+    symbols = dict()
+    for kw in kws:
+        count[kw] = 0
+    with open(filename, 'r') as file:
+        contents = file.read()
+        for kw in kws:
+            count[kw] = len(re.findall(kw, contents))
+        sanitizedContents = contents.replace(" ", "").replace("\t", "").replace("\n", "")
+        totalKeywords = sum(count.values()) * 1.0
+        if totalKeywords != 0:
+            for kw in kws:
+                count[kw] /= totalKeywords
+
+        for sym in sanitizedContents:
+           symbols[sym] = symbols.get(sym, 0) + 1
+
+        totalLength = len(sanitizedContents) * 1.
+        if totalLength != 0:
+            for sym in symbols:
+                symbols[sym] /= totalLength
+
+    return count
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.stderr.write("usage: progname dictionary_file file_to_count_kws")
     else:
-        print countKeywords(readList(sys.argv[1]), sys.argv[2])
+        print countKeywordsAndSymbols(readList(sys.argv[1]), sys.argv[2])
