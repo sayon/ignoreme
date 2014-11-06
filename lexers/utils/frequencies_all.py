32b4fd96160f9dec14dc034b8ede57200322d330
sayon <igorjirkov@gmail.com>
2014-11-07 01:41:33 +0400

    new version of csv and refactorings

--- lexers/utils/frequencies_all.py
+++ lexers/utils/frequencies_all.py
@@ -1,83 +1,34 @@
 import os
 from os.path import join
-import re
-import mmap
 
-def quote(s):
-    s = str(s)
-    if ("\"" in s): s = s.replace("\"", "\"\"")
-    if ( "," in s or ";" in s):
-        return "\"" + s + "\""
-    else:
-        return s
-
-
-def getOrZero(dic, key):
-    if not key in dic.keys():
-        return 0
-    return dic[key]
-
-
-
-def countlines(f):
-    buf = mmap.mmap(f.fileno(), 0)
-    lines = 0
-    readline = buf.readline
-    while readline():
-        lines += 1
-    return lines
-
-def readList(filename):
-    f = open(filename)
-    res = list(map(lambda w: w.replace("\n", ""), f.readlines()))
-    f.close()
-    return res
+from common import readList,  quote, keywordsAndSymbolsFrequencies
 
 
 languages = readList("langs.txt")
 results = dict()
 
 for language in languages:
-    print "Language: " , language
-    files = [ f for f in os.listdir(language) if os.path.isfile(join(language,f)) ]
-    # print "Files: ", files
-
-    kws = dict()
-    for kw in readList(language+"_keywords.txt"):
-        kws[kw] = 0
-
+    print "Language: ", language
+    files = [f for f in os.listdir(language) if os.path.isfile(join(language, f))]
+    fileno = 1
+    kws = readList(language + "_keywords.txt")
 
     for filename in files:
-        features = dict()
-        file = open(language + "/" + filename, 'r')
-        contents = file.read()
-        length_total = 0
-        for kw in kws.keys():
-            features[kw] = len( re.findall(kw, contents ))
-        for sym in contents:
-            if sym != ' ' and sym != '\t' and sym != '\n':
-                features[sym] = getOrZero(features, sym ) + 1
-                length_total += 1
+        print filename, " file ", fileno, " out of ", len(files)
+        fileno += 1
 
-        for w in features.keys():
-            if length_total != 0 : features[w] = features[w] * 1. / length_total
-        file.close()
+        with open(language + "/" + filename, 'r') as file:
+             features = keywordsAndSymbolsFrequencies(kws, file.read())
+
         results[filename] = (features, language)
 
+allFeatures = reduce(lambda x, y: x | set(y[0].keys()), results.values(), set())
 
-allfeatures = reduce(lambda x, y: x | set(y[0].keys()) , results.values(), set())
-allfeatures = filter(lambda x: not '\0' in x, allfeatures)
-out = open("out.csv", "w")
-
-out.write("name," + ",".join(map(quote, allfeatures)) + "\n")
-
-for entry in results.keys():
-    out.write(results[entry][1])
-    for feature in allfeatures:
-        if '\0' not in feature:
-            out.write("," + str(getOrZero(results[entry][0], feature)))
-    out.write("\n")
-
-
-out.close()
+with open("out_nonalpha.csv", "w") as out:
+    out.write("name," + ",".join(map(quote, allFeatures)) + "\n")
+    for entry in results.keys():
+        out.write(results[entry][1])
+        for feature in allFeatures:
+                out.write("," + str( results[entry][0].get(feature, 0)))
+        out.write("\n")
 
--- lexers/utils/frequencies_kw_input.py
+++ lexers/utils/frequencies_kw_input.py
@@ -1,5 +1,7 @@
 import re
 import sys
+from lexers.utils.common import keywordsAndSymbolsFrequencies
+
 
 def readList(filename):
     f = open(filename)
@@ -24,29 +26,8 @@ def countKeywords(kws, filename):
 
 
 def countKeywordsAndSymbols(kws, filename):
-    kwCount = dict()
-    symCount = dict()
-    for kw in kws:
-        kwCount[kw] = 0
     with open(filename, 'r') as file:
-        contents = file.read()
-        for kw in kws:
-            kwCount[kw] = len(re.findall(kw, contents))
-        sanitizedContents = contents.replace(" ", "").replace("\t", "").replace("\n", "")
-        totalKeywords = sum(kwCount.values()) * 1.0
-        if totalKeywords != 0:
-            for kw in kws:
-                kwCount[kw] /= totalKeywords
-
-        for sym in sanitizedContents:
-           symCount[sym] = symCount.get(sym, 0) + 1
-
-        totalLength = len(sanitizedContents) * 1.
-        if totalLength != 0:
-            for sym in symCount:
-                symCount[sym] /= totalLength
-
-    return dict(symCount, **kwCount)
+        return keywordsAndSymbolsFrequencies(kws, file.read())
 
 
 if __name__ == "__main__":
